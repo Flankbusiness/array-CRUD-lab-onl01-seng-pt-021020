f2ec973c237497fa4f0d19294794fa87de6df201
Steven Prindle <flankbusiness@gmail.com>
2020-02-18 03:40:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,6 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
  ["I", "am", "really", "learning"].shift ("wow")
+ add_element_to_start_of_array
 end
 
 def remove_element_from_end_of_array(array)
